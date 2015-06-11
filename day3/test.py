1b5750942b300c5cd998e5f19ef47bbf49865add
Anil1802 <Anilwith786@gmail.com>
2015-06-11 18:33:29 +0530

    3 day of pyhton

---
+++ day3/test.py
@@ -0,0 +1,5 @@
+emp1=int(input("Employee1:how many days"))
+for i in emp1:
+	hour1=int(input("hours? "))
+	sum=sum+hour1
+print("emp1's total hours ",sum)
---
+++ test.py
@@ -0,0 +1,5 @@
+emp1=int(input("Employee1:how many days"))
+for i in range(emp1):
+	hour1=int(input("hours? "))
+	sum1=sum1+hour1
+print("emp1's total hours ",sum1)
