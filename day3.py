1ef9e6ee1ed1abf42d4d05b9b444c64261998adf
Anil1802 <Anilwith786@gmail.com>
2015-06-11 18:49:49 +0530

    3 day

---
+++ day3.py
@@ -0,0 +1,9 @@
+# for i in "susheela":
+# 	print(i) 
+
+
+# name=input("what is ur name?")
+# print("your name is "+ name)
+
+age=int(input("what is ur age ?"))
+print("your age is ",65-age,"years until retirement")
---
+++ stringloop.py
@@ -0,0 +1,2 @@
+for i in "susheela":
+	print(i) 
